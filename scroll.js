6d64b89dc826cf2b963f6ffc39617bcb96ba9e7e
Jagdish <jagdishchoudhary8570@gmail.com>
2021-08-03 19:25:48 +0530

    added files

--- scroll.js
+++ scroll.js
@@ -3,10 +3,10 @@ console.log('hello')
 $(document).ready(function(){
     $(window).scroll(function(){
         if($(window).scrollTop() > 150){
-            $(".navbar").css({"background-color":"#0D362E"});
+            $("nav").css({"background-color":"#0D362E"});
         }
         else{
-            $(".navbar").css({"background-color":""});
+            $("nav").css({"background-color":""});
         }
     })
 })
